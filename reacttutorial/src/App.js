26e7ebcdb0a88e3cb4704d39c18ed095be9b1a72
UzairIshfaq1234 <uzairishfaq1234@gmail.com>
2022-08-20 13:49:15 +0500

    changeds

--- reacttutorial/src/App.js
+++ reacttutorial/src/App.js
@@ -1,45 +1,48 @@
 import { useState } from 'react';
 import './App.css';
+import About from './components/About';
 import Aleart from './components/Aleart';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [mode, setdarkMode] = useState('light');
   const [alert, setalert] = useState(null);
 
-  const  showAlert=(typ,msg)=>
-  {
+  const showAlert = (typ, msg) => {
     setalert({
-      type:typ,
-      message:msg
+      type: typ,
+      message: msg
     })
 
     setTimeout(() => {
 
       setalert(null);
-      
+
     }, 2000);
 
   }
 
 
-  const  togglemode=()=>
-  {
-    if(mode==='light')
-    {
+  const togglemode = () => {
+    if (mode === 'light') {
       setdarkMode('dark');
-      document.body.style.backgroundColor='black';
-      showAlert('success','Dark Mode Enabled')
-      document.title='Darkcoderz! Dark-mode';
+      document.body.style.backgroundColor = 'black';
+      showAlert('success', 'Dark Mode Enabled')
+      document.title = 'Darkcoderz! Dark-mode';
     }
-    else
-    {
+    else {
       setdarkMode('light');
-      document.body.style.backgroundColor='white';
-      showAlert('danger','Dark Mode Disabled')
-      document.title='Darkcoderz! Light-mode';
+      document.body.style.backgroundColor = 'white';
+      showAlert('danger', 'Dark Mode Disabled')
+      document.title = 'Darkcoderz! Light-mode';
 
 
     }
@@ -49,14 +52,40 @@ function App() {
 
   return (
     <>
-      <Navbar title="DARK CODERZ-PK" userName="Muhammad Uzair" mode={mode} togglemode={togglemode}></Navbar>
+      {/* <Navbar title="DARK CODERZ-PK" userName="Muhammad Uzair" mode={mode} togglemode={togglemode}></Navbar> */}
       {/* <Navbar ></Navbar> */}
 
-      <div className="container">
+      {/* <div className="container">
 
-      <Aleart alert={alert}></Aleart>
+        <Aleart alert={alert}></Aleart>
         <TextForm showAlert={showAlert} head="Enter Text For Operation!" mode={mode}></TextForm>
-      </div>
+      </div> */}
+
+      {/* <div className="container">
+        <About></About>
+      </div> */}
+
+      <Router>
+
+        <Switch>
+          {/* ------------------------Start form  ---------------------*/}
+          <Route path="/">
+            <Navbar title="DARK CODERZ-PK" userName="Muhammad Uzair" mode={mode} togglemode={togglemode}></Navbar>
+            <div className="container">
+              <Aleart alert={alert}></Aleart>
+              <TextForm showAlert={showAlert} head="Enter Text For Operation!" mode={mode}></TextForm>
+            </div>
+          </Route>
+          {/* ------------------------about form  ---------------------*/}
+          <Route path="/about">
+            <Navbar title="DARK CODERZ-PK" userName="Muhammad Uzair" mode={mode} togglemode={togglemode}></Navbar>
+            <div className="container">
+              <About></About>
+            </div>
+          </Route>
+          {/* ------------------------End form  ---------------------*/}
+        </Switch>
+      </Router>
 
     </>
 
---
+++ reacttutorial/src/components/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <div className="container">
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est facere pariatur possimus quae sequi eos earum accusamus aperiam, ducimus, obcaecati veritatis, repellendus quidem vel fugit quas! Voluptatum consectetur repellendus necessitatibus.</p>
+            </div>
+        </div>
+    )
+}
+
+export default About
+
+
+
+
+
+
